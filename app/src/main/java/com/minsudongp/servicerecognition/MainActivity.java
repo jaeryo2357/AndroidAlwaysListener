c692698f1b8f86ed72c1453fc9040098edad5e48
jaeryo2357 <jaeryo2357@naver.com>
2019-03-28 20:53:33 +0900

    실시간 음성인식

---
+++ app/src/main/java/com/minsudongp/servicerecognition/MainActivity.java
@@ -0,0 +1,26 @@
+package com.minsudongp.servicerecognition;
+
+import android.content.Intent;
+import android.speech.RecognitionListener;
+import android.speech.RecognitionService;
+import android.speech.RecognizerIntent;
+import android.speech.SpeechRecognizer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        startService(new Intent(this,Recoginition.class));
+    }
+
+
+}
